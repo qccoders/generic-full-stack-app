5064160c4a02bd175f1817ec335f4c0aeb6c546b
JP Dillingham <jpdillingham@gmail.com>
2020-03-25 18:27:04 -0500

    fix everything

--- api/src/index.js
+++ api/src/index.js
@@ -6,7 +6,7 @@ const { port } = require('./config');
 const app = express();
 
 app.use(cors());
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '../public')))
 
 const router = express.Router();
 app.use('/api', router);
